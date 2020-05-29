121eedcf398495b362e846d5bcbdafe5fa43eead
leolee <q441987091@gmail.com>
2020-05-29 19:43:47 +0800

    finish cart Nav

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,13 +1,24 @@
-<style>
-
+<style scoped>
+  .Navcart {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
 <template>
-    <h2>
-      购物车
-    </h2>
+   <div class="cart">
+     <nav-bar class="Navcart">
+      <template v-slot:center>
+        <div > 购物车</div>
+      </template>
+     </nav-bar>
+   </div>
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  name : 'Cart'
+  name : 'Cart',
+  components: {
+    NavBar
+  }
 }
 </script>
